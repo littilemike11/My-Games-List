c80f72ccbac1330616ab6b9b972ea1c775cd5935
Michael <michaeledquilan@gmail.com>
2025-08-08 11:58:39 -0400

    added navbar routes

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -14,80 +14,145 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-1">
-        <Link href={"/"} className="btn  btn-ghost text-xl">
-          My Games List
-        </Link>
-      </div>
-      <div className="flex gap-2">
-        <Link href={""} className="link link-hover">
-          Contact
-        </Link>
-        <Link href={"/reviews"} className="link link-hover">
-          Reviews
-        </Link>
-        <Link href={"/discussions"} className="link link-hover">
-          Discussions
-        </Link>
-        <Link href={"/lists"} className="link link-hover">
-          Lists
-        </Link>
-        <Link href={"/player"} className="link link-hover">
-          Players
-        </Link>
-        <Link href={"/articles"} className="link link-hover">
-          Articles
-        </Link>
-        <input
-          type="text"
-          placeholder="Search"
-          className="input input-bordered w-24 md:w-auto"
-        />
-        <AuthModal isOpen={showAuth} onClose={() => setShowAuth(false)} />
-
-        {session ? (
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <Link href={"/"} className="btn  btn-ghost text-xl">
+            My Games List
+          </Link>{" "}
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm space-y-2 dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <p>Welcome {profile?.username}! </p>
               <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
+                <Link href={"/reviews"} className="link link-hover">
+                  Reviews
+                </Link>
               </li>
               <li>
-                <a>Settings</a>
+                <Link href={"/discussions"} className="link link-hover">
+                  Discussions
+                </Link>
               </li>
               <li>
-                <button onClick={logout} className="btn">
-                  Logout
-                </button>
+                <Link href={"/lists"} className="link link-hover">
+                  Lists
+                </Link>
+              </li>
+              <li>
+                <Link href={"/player"} className="link link-hover">
+                  Players
+                </Link>
+              </li>
+              <li>
+                <Link href={"/articles"} className="link link-hover">
+                  Articles
+                </Link>
               </li>
             </ul>
           </div>
-        ) : (
-          <button className="btn btn-primary" onClick={() => setShowAuth(true)}>
-            Sign In
-          </button>
-        )}
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link href={"/reviews"} className="link link-hover">
+                Reviews
+              </Link>
+            </li>
+            <li>
+              <Link href={"/discussions"} className="link link-hover">
+                Discussions
+              </Link>
+            </li>
+            <li>
+              <Link href={"/lists"} className="link link-hover">
+                Lists
+              </Link>
+            </li>
+            <li>
+              <Link href={"/player"} className="link link-hover">
+                Players
+              </Link>
+            </li>
+            <li>
+              <Link href={"/articles"} className="link link-hover">
+                Articles
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end gap-2">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+          <AuthModal isOpen={showAuth} onClose={() => setShowAuth(false)} />
+
+          {session ? (
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm space-y-2 dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                <p>Welcome {profile?.username}! </p>
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <button onClick={logout} className="btn">
+                    Logout
+                  </button>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <button
+              className="btn btn-primary"
+              onClick={() => setShowAuth(true)}
+            >
+              Sign In
+            </button>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Navbar;
