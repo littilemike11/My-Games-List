441b0f2989d567daecec9f72e5d0df67cbaa50da
Michael <michaeledquilan@gmail.com>
2025-09-19 12:42:06 -0400

    created explore layout

---
+++ frontend/app/explore/page.tsx
@@ -0,0 +1,12 @@
+import { getReviews } from "../api/supabase-api/review-api";
+import { getDiscussions } from "../api/supabase-api/discussion-api";
+import { getLists } from "../api/supabase-api/list-api";
+const ExplorePage = () => {
+  return (
+    <>
+      <h1>welcome to explore pg</h1>
+    </>
+  );
+};
+
+export default ExplorePage;
