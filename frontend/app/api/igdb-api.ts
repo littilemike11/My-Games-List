f0f0665632103b9ec903b210b88b53a63924a739
Michael <michaeledquilan@gmail.com>
2026-02-06 01:06:45 -0500

    testing games api

--- frontend/app/api/igdb-api.ts
+++ frontend/app/api/igdb-api.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 import { Game } from "../types/models";
 
 const getGames = async (body: string): Promise<Game[]> => {
-  const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL
+    ? "https://api.igdb.com/v4/games"
+    : "http://localhost:3000/api/games";
   try {
-    const response = await axios.post(`${BASE_URL}/api/games`, body, {
+    const response = await axios.post(`${BASE_URL}`, body, {
       headers: {
         "Content-Type": "text/plain", // IMPORTANT
       },
